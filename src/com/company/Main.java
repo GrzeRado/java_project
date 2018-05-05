fe4e9caebe5b3cfb9712712e1d9e0c717f60b5d5
Grzegorz Radomski <rc36@wp.pl>
2018-05-05 04:34:21 +0200

    initialized new variable called whatever

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("First steps in Java");
+        int liczba = 10;
+        System.out.println(liczba);
     }
 }
