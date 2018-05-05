7a31ae936b2293c8fa4ac831e1580d2375806054
Grzegorz Radomski <rc36@wp.pl>
2018-05-05 16:56:16 +0200

    add switch into code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("First steps in Java");
         int liczba = 10;
         System.out.println(liczba);
+
+        //insert switch
+
+        switch {
+
+            case 1:
+                (int>=1 "answer is true");
+                break;
+            case 2:
+                (int>=2 "you grow be hero");
+                break;
+                default "nice job, good luck";
+        }
+
     }
 }
